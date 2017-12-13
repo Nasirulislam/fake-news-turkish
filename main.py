872f60109a7ab4bf07614a99b3b3e20a27f340fd
praveshkoirala@gmail.com <praveshkoirala@gmail.com>
2017-12-13 08:03:50 +0545

    CSV of the file and a preliminary result using SVM

--- main.py
+++ main.py
@@ -3,9 +3,13 @@
 Fake news detector for Turkish language
 """
 from urlparse import urlparse
-
+import scipy as sp
 import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.decomposition import PCA, TruncatedSVD
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.grid_search import GridSearchCV
+from sklearn.preprocessing import normalize
+from sklearn.svm import SVC
 import pytest
 import snowballstemmer
 from nltk.tokenize import RegexpTokenizer
@@ -17,8 +21,11 @@ stop_word = StopWords()
 def read_file_xlsx(file_path):
     # check that the file is indeed an excel file
     assert file_path[-5:] == ".xlsx"
-    df = pd.read_excel(file_path, index_col=0)
+    df = pd.read_excel(file_path, index_col=0, encoding="utf-8")
     return df
+
+def read_file_csv(file_path):
+    return pd.read_csv(file_path, index_col=0, encoding="utf-8")
 
 
 def stem_turkish_text(stemmer, text):
@@ -38,7 +45,7 @@ def transform_df(df):
     df = df.loc[:, ["NewsDate", "Url", "NewsTitle", "News", "Value"]]
 
     # transform NewsDate column to a boolean indicating whether the date is present or not
-    values = df["NewsDate"] != pd.NaT
+    values = (df["NewsDate"] != pd.NaT) * 1
     df["NewsDate"] = values
 
     # transform Url column to just the hostname or "Social" if the news source is social media
@@ -66,13 +73,37 @@ def to_features(df, feature):
     # first retrieve the corpus
     corpus=list(df["NewsTitle"].str.split()) + list(df["News"].str.split())
     corpus = reduce(list.__add__, corpus)
-    model = CountVectorizer().fit(corpus)
-    return model.transform(df["NewsTitle"])
-
+    vectorizer = CountVectorizer().fit(corpus)
+    news_title_sparse = vectorizer.transform(df["NewsTitle"])
+    news_sparse = vectorizer.transform(df["News"])
+    X = sp.sparse.hstack((news_title_sparse, news_sparse), format='csr')
+    return normalize(X)
 
 
 if __name__ == "__main__":
-    df = read_file_xlsx("TDFFN.xlsx")
-    print to_features(df, "")
-    # df = transform_df(df)
-    # print df.head(10)
+    df = read_file_csv("TDFFN.csv")
+    df = transform_df(df)
+    X =  to_features(df, "")
+    pca = TruncatedSVD(20, n_iter=10).fit(X)
+    print pca.explained_variance_ratio_.sum() * 100
+    X = normalize(pca.transform(X))
+    # exit()
+    # train
+
+    # Set the parameters by cross-validation
+    tuned_parameters = [{'kernel': ['rbf', 'linear'], 'gamma': [1e-3, 1e-4],
+                         'C': [1, 10, 100, 1000]},
+                        {'kernel': ['linear'], 'C': [1, 10, 100, 1000]}]
+
+    scores = ['precision', 'recall']
+
+    clf = GridSearchCV(SVC(), tuned_parameters, cv=5,
+                           scoring='%s_macro' % 'recall')
+    Y = df["Value"].values
+    clf.fit(X, Y)
+    print clf.best_params_
+    print
+    print "Grid scores on development set:"
+    print
+    print clf.best_estimator_
+    print clf.best_score_
